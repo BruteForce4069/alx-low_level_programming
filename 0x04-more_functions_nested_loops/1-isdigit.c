f71864005958803a2d7da4419ef140070d78bc3e
BruteForce4069 <122855924+BruteForce4069@users.noreply.github.com>
2023-02-23 18:38:18 -0800

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+
+int _isdigit(int x)
+
+{
+	if (x >= 0 && x <= 100)
+	{
+	return (1);
+	}
+	return (0);
+}
